ae8a0f59a68ce31cfe886a8bae78a7a1f4ccac7f
Muhammad Vohra <muhammad.vohra@oaknorth.co.uk>
2023-12-21 19:34:05 +0000

    feat: styled ui components for new fields

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -1,13 +1,22 @@
 "use client";
 
 import { Project as ProjectModel } from "@prisma/client";
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "./ui/card";
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "./ui/card";
 import { useState } from "react";
 import AddEditProjectDialog from "./AddEditProjectDialog";
+import { Clock, FlagTriangleRight } from "lucide-react";
+import { Button } from "./ui/button";
 
 interface ProjectProps {
     project: ProjectModel;
 }
+
+const deadlineApproching = (deadline: Date) => {
+    const now = new Date();
+    const diff = deadline.getTime() - now.getTime();
+    const days = diff / (1000 * 60 * 60 * 24);
+    return days;
+};
 
 const Project = ({project}: ProjectProps) => {
     const [showEditProjectDialog, setShowEditProjectDialog] = useState(false);
@@ -23,21 +32,35 @@ const Project = ({project}: ProjectProps) => {
                 }}
             >
                 <CardHeader>
-                    <CardTitle>
+                    <CardTitle className="flex items-center justify-between">
                         {project.title}
+
+                        {project.priority !== 'low' &&
+                            <FlagTriangleRight color={project.priority == 'high' ? 'red' : 'orange'} />
+                        }
+                        
                     </CardTitle>
-                    <CardDescription>
-                        {date}
-                        {wasUpdated && " (updated)"}
+                    <CardDescription className="flex flex-col gap-1 mt-4">
+                        <div>
+                            {date}
+                            {wasUpdated && " (updated)"}
+                        </div>
+                        { project.deadline &&
+                            <div className={`flex gap-2 items-center ${deadlineApproching(project.deadline) < 3 ? 'text-red-500' : 'text-gray-500'}`}>
+                                <Clock size={16} />
+                                <p className="">
+                                    {project.deadline?.toDateString()}
+                                </p>
+                            </div>
+                        }
                     </CardDescription>
                 </CardHeader>
                 <CardContent>
-                    <p className="whitespace-pre-line">{project.content}</p>
-                    <p className="whitespace-pre-line">{project.status}</p>
-                    <p className="whitespace-pre-line">{project.priority}</p>
-                    <p className="whitespace-pre-line">{project.deadline?.toDateString()}</p>
-                    
+                    <p className="whitespace-pre-line">{project.content}</p>    
                 </CardContent>
+                <CardFooter className="flex items-center justify-end">
+                    <span className={`text-right capitalize p-2 rounded-lg ${project.status == 'done' ? 'bg-green-400' : 'bg-gray-200'}`}>{project.status?.replace("-", " ")}</span>
+                </CardFooter>
             </Card>
             <AddEditProjectDialog open={showEditProjectDialog} setOpen={setShowEditProjectDialog} projectToEdit={project}/>
         </>
